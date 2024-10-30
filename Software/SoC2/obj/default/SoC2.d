0ac9a6a6fc7c397852a3e932920435e262b5ee4e
Dias <gabriel.dias@unipampa.edu.br>
2024-10-29 22:24:55 -0300

    project: update files

---
+++ Software/SoC2/obj/default/SoC2.d
@@ -0,0 +1,10 @@
+obj/default/SoC2.o: SoC2.c ../SoC2_bsp/system.h ../SoC2_bsp/linker.h \
+  ../SoC2_bsp//HAL/inc/io.h ../SoC2_bsp//HAL/inc/alt_types.h
+
+../SoC2_bsp/system.h:
+
+../SoC2_bsp/linker.h:
+
+../SoC2_bsp//HAL/inc/io.h:
+
+../SoC2_bsp//HAL/inc/alt_types.h:
