433839754ba6f7d485139e4a71b2025bd37beab3
Dias <gabriel.dias@unipampa.edu.br>
2024-10-30 11:37:29 -0300

    project: refactor dir tree

---
+++ software/SoC2_workspace/SoC2/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c \
+  C:/PSE/FisrtProjetcPSE/software/SoC2_workspace/SoC2_bsp/system.h \
+  C:/PSE/FisrtProjetcPSE/software/SoC2_workspace/SoC2_bsp/linker.h \
+  C:/PSE/FisrtProjetcPSE/software/SoC2_workspace/SoC2_bsp//HAL/inc/io.h \
+  C:/PSE/FisrtProjetcPSE/software/SoC2_workspace/SoC2_bsp//HAL/inc/alt_types.h
+
+C:/PSE/FisrtProjetcPSE/software/SoC2_workspace/SoC2_bsp/system.h:
+
+C:/PSE/FisrtProjetcPSE/software/SoC2_workspace/SoC2_bsp/linker.h:
+
+C:/PSE/FisrtProjetcPSE/software/SoC2_workspace/SoC2_bsp//HAL/inc/io.h:
+
+C:/PSE/FisrtProjetcPSE/software/SoC2_workspace/SoC2_bsp//HAL/inc/alt_types.h:
